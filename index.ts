9f200a4e8bfbcc35dd7dff9b4737aa4adaec5b47
Semen <bethesda.stillxxxx@gmail.com>
2021-04-03 12:06:10 +0000

    Fix

--- index.ts
+++ index.ts
@@ -29,8 +29,8 @@ app.use('/api', routes)
 
 
 const httpsOptions = {
-    cert: fs.readFileSync(path.join(__dirname, '..', 'server.crt')),
-    key: fs.readFileSync(path.join(__dirname, '..', 'server.key')),
+    cert: fs.readFileSync(path.join(String(process.env.HOME_DIR), '..', 'server.crt')),
+    key: fs.readFileSync(path.join(String(process.env.HOME_DIR), '..', 'server.key')),
 }
 
 https.createServer(httpsOptions, app)
