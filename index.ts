24b3847e0a08960099133cb588cecd9c0892b5e2
semen <bethesda.stillxxxx@gmail.com>
2021-04-03 13:00:59 +0300

    Теперь запуск по протоколу https

--- index.ts
+++ index.ts
@@ -1,3 +1,4 @@
+import https from "https"
 import dotenv from "dotenv"
 import express from "express"
 import bodyParser from "body-parser"
@@ -9,6 +10,7 @@ dotenv.config()
 
 import routes from "./src/routes/routes";
 import { middlewares, passport } from "./src/lib/index"
+import fs from "fs"
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
@@ -24,7 +26,12 @@ app.use(passport.session())
 app.use(express.static(__dirname + '/public'))
 app.use('/api', routes)
 
+const httpsOptions = {
+    cert: fs.readFileSync(String(process.env.HTTPS_KEY)),
+    key: fs.readFileSync(String(process.env.HTTPS_CERT)),
+}
 
-app.listen(process.env.PORT, () => {
+https.createServer(httpsOptions, app)
+.listen(process.env.PORT, () => {
     console.log('Server started...')
 })
