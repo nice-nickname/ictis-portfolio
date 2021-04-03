a10d9a357235368f741652677522277a388ae496
Semen <bethesda.stillxxxx@gmail.com>
2021-04-03 10:04:00 +0000

    Добавил подмодуль сайта

--- index.ts
+++ index.ts
@@ -21,7 +21,7 @@ app.use(middlewares.enableCors)
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(express.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public/test'))
 app.use('/api', routes)
 
 
